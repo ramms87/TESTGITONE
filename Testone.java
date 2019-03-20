04327b16d5840826f46e99acc9b9ed13ebede631
RAMMS87 <ramms2387@gmail.com>
2019-03-20 01:24:55 -0400

    Updated the trial version of Testone.java file

---
+++ Testone.java
@@ -0,0 +1,6 @@
+total 28
+drwxr-xr-x 1 CVS+C042954 2147484161  0 Mar 20 00:48 ./
+drwxr-xr-x 1 CVS+C042954 2147484161  0 Mar 20 00:27 ../
+drwxr-xr-x 1 CVS+C042954 2147484161  0 Mar 20 00:17 .git/
+-rw-r--r-- 1 CVS+C042954 2147484161 12 Mar 20 00:17 README.md
+-rw-r--r-- 1 CVS+C042954 2147484161  0 Mar 20 00:48 Testone.java
